70e5fbfb0236239f7f4293d9535e61c8dade6e41
AinoMegumi <aimegu2014@gmail.com>
2022-07-20 12:43:39 +0900

    APIエンドポイント用クラスを追加

---
+++ TrafficController/TrafficController/Controllers/ApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrafficController.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+
+    }
+}
